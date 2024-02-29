e89b69f85ae9b047ab12033ad8affd8940e8225a
Anmol Tyagi <anmol.tyagi@gosamplify.com>
2024-02-29 23:22:37 +0530

    updates 2.0

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 app.use("/api/contact", contactRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
